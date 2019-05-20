ec2205c9ea56efa87108fb5394ba5dcb7c9e3114
Andres Loja <aloja619@gmail.com>
2019-05-20 13:43:19 -0500

    Visualizar

--- mostrar.js
+++ mostrar.js
@@ -1,9 +1,54 @@
-function iniciar(){
+lista = [{ "id": 1, "imagen": "Imagenes/I1.jpg" },
+{ "id": 2, "imagen": "Imagenes/I2.jpg" },
+{ "id": 3, "imagen": "Imagenes/I3.jpg" },
+{ "id": 4, "imagen": "Imagenes/I4.jpg" },
+{ "id": 5, "imagen": "Imagenes/I5.jpg" },
+{ "id": 6, "imagen": "Imagenes/I6.jpg" },
+{ "id": 7, "imagen": "Imagenes/I7.jpg" },
+{ "id": 8, "imagen": "Imagenes/I8.jpg" },
+{ "id": 9, "imagen": "Imagenes/I9.jpg" },
+{ "id": 10, "imagen": "Imagenes/I10.jpg" }];
+imagenes = [1, 2, 3, 4, 5];
+var posActual = 0;
 
-}
-function atras(){
 
+function inicio() {
+    for (i = 0; i < 5; i++) {
+        imagenes[i] = Math.floor(Math.random() * (10));
+
+    }
+    posActual = 0;
+    verificar();
+    imprimir();
 }
-function siguiente(){
-    
+
+function imprimir() {
+    console.log(imagenes);
+    var texto = "<img src=" + lista[imagenes[posActual]].imagen + ">";
+    window.document.getElementById("imagen").innerHTML = texto;
+}
+function verificar() {
+    if (posActual == 4) {
+        window.document.getElementById("btn_siguiente").disabled = true;
+        window.document.getElementById("btn_anterior").disabled = false;
+    }
+    else if (posActual == 0) {
+        window.document.getElementById("btn_anterior").disabled = true;
+        window.document.getElementById("btn_siguiente").disabled = false;
+    } else {
+        window.document.getElementById("btn_siguiente").disabled = false;
+        window.document.getElementById("btn_anterior").disabled = false;
+    }
+}
+
+function anterior() {
+    posActual--;
+    verificar();
+    imprimir();
+}
+
+function siguiente() {
+    posActual++;
+    verificar();
+    imprimir();
 }
